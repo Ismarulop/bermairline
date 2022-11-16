07d43cf4c2448d0e2914aabb3f4a3605bea8277f
Bernardo-Pinto <bernardo.ra.pinto@gmail.com>
2022-11-16 12:06:17 +0100

    Basic dropdown lists and fetch for flight origins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect  } from "react";
+import "./bootstrap.css";
 
 function App() {
+
+  const [result,setResult] = useState([]);
+  const [posts, setPosts] = useState([]);
+  // fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+  //  .then(response => response.json())
+  //  .then(data => console.log(data))
+  //  .then(data => setResult(data));
+
+   useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+          setPosts(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>BERMA Airlines</h1>
+      <h2>Let your life fly</h2>
+      <form>
+        <fieldset>
+          <legend>Flight Information</legend>
+          <div className="form-group">
+            <label htmlFor="exampleSelect1" className="form-label mt-4">
+              {posts[0].title}
+            </label>
+            <select className="form-select" id="exampleSelect1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleSelect1" className="form-label mt-4">
+            1 Way or Round Trip?
+            </label>
+            <select className="form-select" id="exampleSelect1">
+              <option>1 Way Trip</option>
+              <option>Round Trip</option>
+            </select>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
